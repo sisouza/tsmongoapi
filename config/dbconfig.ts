cbd67bbd7c958f369ec11edd156fd84d2131c54d
sisouza <sidia.reis1@gmail.com>
2021-05-02 13:21:33 -0400

    fixing db connection

--- config/dbconfig.ts
+++ config/dbconfig.ts
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
 import { MONGO_URL } from '../env';
 
-export default const dbconfig = mongoose.connect(MONGO_URL, {
-    useCreateIndex: true,
-    useNewUrlParser: true
-}, () => {
-    console.log('db connected')
-})
+const dbconfig = async () => {
+    await mongoose.connect(MONGO_URL, {
+        useCreateIndex: true,
+        useNewUrlParser: true
+    })
+}
+
+export default dbconfig;
