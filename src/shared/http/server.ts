28438b85679d5ea8e91413812bf190cfc78b00d9
sisouza <sidia.reis1@gmail.com>
2021-05-01 22:24:54 -0400

    configuring path mapping

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,15 +1,29 @@
-import express from 'express';
-
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import AppError from '@shared/error/AppError';
 
 const app = express();
-const porta = 3000;
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+
+app.use(express.json());
+app.use(errors());
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  console.log(error);
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 });
 
-app.listen(porta, err => {
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`servidor rodando na porta ${porta}`);
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
 });
