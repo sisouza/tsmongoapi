d85defc8d5ff04b4c9a80b1f047cb94046a77303
sisouza <sidia.reis1@gmail.com>
2021-05-01 23:46:38 -0400

    ' add async error'

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,11 @@
-import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-import AppError from '@shared/error/AppError';
+import AppError from '../error/AppError';
 
 const app = express();
 
 app.use(express.json());
-app.use(errors());
+// app.use(error());
 
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
